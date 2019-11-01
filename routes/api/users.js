947f386f757d92d67a238aeeee100e39eb3ff2af
Frank Fang <frankfang2014@hotmail.com>
2019-11-01 16:50:54 -0700

    user register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bryct = require("bcryptjs");
+
+const User = require("../../models/User");
+
 //@router Get api/users
 //@desc Resigter user
 //@access Public
@@ -16,12 +21,33 @@ router.post(
       "Please enter a password with 6 or more characters "
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exist" }] });
+      }
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+      user = new User({ name, email, password, avatar });
+      const salt = await bryct.genSalt(10);
+      user.password = await bryct.hash(password, salt);
+
+      await user.save();
+
+      res.send("User registered");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("server error");
+    }
   }
 );
 
