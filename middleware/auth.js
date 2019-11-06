3c0fab40819fd1bdc29aa12a42881bacc6a343c9
Frank Fang <frankfang2014@hotmail.com>
2019-11-05 17:03:20 -0800

    create and update profile

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function(req, res, next) {
-  const token = req.header("x-auto-token");
+  const token = req.header("x-auth-token");
   if (!token) {
     return res.status(401).json({ msg: "no token,auth denied" });
   }
