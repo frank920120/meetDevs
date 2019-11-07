f62185c748a051e3c38caf0e781a894e117a5fa9
Frank Fang <frankfang2014@hotmail.com>
2019-11-06 16:37:54 -0800

    create delete experience router

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -194,5 +194,24 @@ router.put(
     }
   }
 );
+//@router delete api/profile/experience/exp_id
+//@desc   delete experience
+//@access private
 
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
