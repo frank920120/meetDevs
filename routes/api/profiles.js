bb52c0505f55d7d423646ae86757a000319a344c
Frank Fang <frankfang2014@hotmail.com>
2019-11-06 16:04:54 -0800

    add profile experience

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -148,4 +148,51 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
+//@router Put api/profile/experience
+//@desc   add experience
+//@access private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is require")
+        .not()
+        .isEmpty(),
+      check("company", "Company is require")
+        .not()
+        .isEmpty(),
+      check("from", "from date is require")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+    const newExp = { title, company, location, from, to, current, description };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
