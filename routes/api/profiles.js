56abc1046d78061cf24feba1f6940719df732324
Frank Fang <frankfang2014@hotmail.com>
2019-11-06 16:44:50 -0800

    add and delete education router

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -214,4 +214,82 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+//@router Put api/profile/education
+//@desc   add education
+//@access private
+
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is require")
+        .not()
+        .isEmpty(),
+      check("degree", "Degree is require")
+        .not()
+        .isEmpty(),
+      check("fieldofstudy", "Fieldofstudy is require")
+        .not()
+        .isEmpty(),
+      check("from", "from date is require")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+//@router delete api/profile/education/exp_id
+//@desc   delete education
+//@access private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
